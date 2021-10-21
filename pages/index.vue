eb430b1ec096f571346f537af81cb4bb17d514fd
hiiiroaqua <plutonium.090301@gmail.com>
2021-10-21 12:53:16 +0900

    chore: update config

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        {{ text }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $microcms }) {
+    const data = await $microcms.get({
+      endpoint: 'hello',
+    });
+    return data;
+  }
+}
+</script>
